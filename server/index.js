ef62e91cc1355ad98b5bb6e2d01977ce9a3524a2
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-05-02 18:01:42 +0700

    initial routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
+//? Requires
 require('dotenv').config({path : "./config.env"});
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
+//? Routes
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use('/api/auth',require('./routes/auth'));
+//? Server starting up
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 })
